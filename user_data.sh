abc24e73b60b389d2b8df5a8918a809b29b242ee
Frank K <dev@franok.de>
2022-01-26 23:05:51 +0100

    update to 2022 guide
    - minecraft 1.18.1
    - java 17

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sudo yum -y install java-1.8.0
+# install java 17 according to https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/amazon-linux-install.html
+sudo yum -y install java-17-amazon-corretto-headless
 sudo yum -y install setools
 sudo systemctl start polkit
 
@@ -7,14 +8,12 @@ sudo systemctl start polkit
 sudo mkdir /minecraft
 sudo mkdir /minecraft/mc-server
 cd /minecraft
-#copy disabled, as we attach existing EBS:
+# copy disabled, as we do not want to overwrite EBS with old version from S3 every time. enable for initial setup:
 #aws s3 cp --recursive s3://<s3-bucket-name>/mc-server/ mc-server/
 sudo chown -R ec2-user:ec2-user /minecraft
 
 # systemd service
-# if using free tier t2.micro instance change name to minecraft512.service, adjust Xms/Xmx flags accordingly and do not use existing EBS
 sudo aws s3 cp s3://<s3-bucket-name>/setup/minecraft.service /etc/systemd/system
 sudo systemctl daemon-reload
-# if using free tier t2.micro instance change name to minecraft512 and do not use existing EBS
 sudo systemctl enable minecraft
 sudo systemctl start minecraft
